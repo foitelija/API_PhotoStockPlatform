8efec81af7dab381531bd90191107530f6f9b611
Nikolay <foitelija777@gmail.comm>
2022-02-16 18:36:50 +0300

    Connected to database Created HttpGet action

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace API_PhotoStockPlatform.Controllers
 {
@@ -13,6 +14,35 @@ namespace API_PhotoStockPlatform.Controllers
     [ApiController]
     public class AuthorController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
 
+        public AuthorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"SELECT * FROM sauthor";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PhotoStockAppCon");
+            SqlDataReader myReader;
+
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(SqlCommand myCommand = new SqlCommand(query,myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }    
+            }
+            return new JsonResult(table);
+        }
     }
 }
