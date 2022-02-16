1d25ffc0b3bef2226472df92ae89225c0ed269f5
Nikolay <foitelija777@gmail.comm>
2022-02-16 20:55:07 +0300

    Added HttPost api method

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using API_PhotoStockPlatform.Models;
 
 namespace API_PhotoStockPlatform.Controllers
 {
@@ -43,6 +44,35 @@ namespace API_PhotoStockPlatform.Controllers
                 }    
             }
             return new JsonResult(table);
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(Author author)
+        {
+            string query = @"INSERT INTO author (name, nickname, age, date_reg) VALUES (@name,@nickname,@age,@date_reg)";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PhotoStockAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@name", author.name);
+                    myCommand.Parameters.AddWithValue("@nickname", author.nickname);
+                    myCommand.Parameters.AddWithValue("@age", author.age);
+                    myCommand.Parameters.AddWithValue("@date_reg", author.date_reg);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added successfully");
         }
     }
 }
