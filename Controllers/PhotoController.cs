87a5a5a2afffb332845ac30548c096aac4247255
Nikolay <foitelija777@gmail.comm>
2022-02-16 21:43:31 +0300

    UPDATE: Getting all Content Entities Photo

--- Controllers/PhotoController.cs
+++ Controllers/PhotoController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using API_PhotoStockPlatform.Models;
 
 namespace API_PhotoStockPlatform.Controllers
 {
@@ -13,6 +15,39 @@ namespace API_PhotoStockPlatform.Controllers
     [ApiController]
     public class PhotoController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public PhotoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"select author.id as 'author ID', author.nickname as 'Nickname',
+            photo.id as 'Photo id', photo.name as 'Photo name', photo.link as 'Photo link', photo.date_add as 'Photo date add',
+            photo.size as 'Photo size', photo.price as 'Price' from author join photo on photo.author_id = author.id ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("PhotoStockAppCon");
+            SqlDataReader myReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
 
     }
 }
